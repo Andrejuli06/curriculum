9f079d718fd419fb2053747dfa4e9193cfd87cb2
Andrejuli06 <acasadiego979@gmail.com>
2024-05-14 15:00:22 -0500

    cargando archivo

--- js/funtcions.js
+++ js/funtcions.js
@@ -2,7 +2,7 @@
 // alert("HOLA SOY UN ALERT DESDE EL ARCUVO EXTERNO DE JS");
 // console.log("hola soy un mensaje de consola");
 // document.write("hola soy la interfas principal");
-Swal.fire("Soy una alerta de una libreria");
+// Swal.fire("Soy una alerta de una libreria");
 
 //variables y tipos de datos
 
@@ -63,7 +63,12 @@ function send_info(){
       }else{
         document.getElementById("last_name").style.background = "green"
       }
-      if(password== password2 ){
+      if(password == password2 ){
+        Swal.fire({
+          title:"Contraseña correcta",
+          Text:"contraseña valida",
+          icon:"valido"
+        });
         document.write("contraseña correcta");
       }else{
         Swal.fire({
@@ -77,4 +82,82 @@ function send_info(){
         document.getElementById("print").innerText = name+" "+last_name;
     }
 
+}
+//ARRAYS 
+var dias_sem=["lunes","martes","miercoles","jueves","viernes", "sabado","domingo"];
+var array_num=[1,2,3,4,5,6,7,8,9];
+var array_mix=[1,2,3, "abc"];
+var array_mul=[
+  {name:"andrea",age: 18,color:"green"},
+  {name:"gerardo", age:18, color:"white"},
+  {name:"mildred",age:46,color:"black"},
+  {name:"valentina",age:14,color:"gray"}
+]
+// es utilizada nosql
+var json_ejm={
+  name:"andrea",
+  lastname:"casadiegos",
+  phone:"3228963551",
+  email:"acasadiego979@gmail.com"
+}
+console.log(dias_sem);
+console.log(array_num);
+console.log(array_mix);
+console.log(array_mul);
+console.log(json_ejm);
+
+for(let i=0; i< dias_sem.length;i++){
+  console.log(dias_sem[i]);
+}
+var j=0;
+while(j<array_num.length){
+  console.log(array_num[j])
+  j++
+}
+var acum=0;
+for(let h = 0;h<array_num.length; h++){
+   acum += array_num[h];
+}
+console.log(acum);
+var acum2=0;
+for(let n=0; n< array_mul.length;n++){
+  acum2 += array_mul[n].age;
+ 
+}
+// document.write(acum2);
+document.getElementById("print_age").innerText = "el valor de la suma es: "+ acum2;
+//value una propiedad que tienen los imput 
+document.getElementById("valores").value =array_num;
+
+// para limpiar una casa de 
+function limpiar(){
+  document.getElementById("valores").value="";
+}
+// array.pop para agregar 
+// let un tipo de variable para funciones 
+var array_ejem =[1,2,3,4,5,6,7,8,9];
+function agregar(){
+  let val_new =array_ejem.push(10);
+  document.getElementById("valores").value=array_ejem;
+}
+// array.push añade un elemento al final 
+// no se puede agragar un elemento a la mitad de array por que daño el array 
+
+function eliminar(){
+  array_ejem.pop()
+  document.getElementById("valores").value="";
+}
+//agregar un imput que escriba un numero en el primero y que elimine el primero  
+function imp_rever(){
+  document.getElementById("text_rever").innerText= array_ejem.reverse();
+  document.getElementById("valores") = false;
+}
+var array_num2 =[1,2,3,4];
+function agregarp(){
+  let val_new1=array_num2.unshift();
+  document.getElementById("valores").value=array_num2;
+}
+function eliminarp(){
+  let val_new2= array_num2.shift();
+  document.getElementById("valores").value="";
 }
